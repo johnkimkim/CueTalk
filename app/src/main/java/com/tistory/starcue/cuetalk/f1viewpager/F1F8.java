e448bb935fdafdc47fb113f273dd89d19d77149c
johnkimkim <kmj654649@gmail.com>
2021-05-20 21:38:00 +0900

    finished f1 viewpager need to make splash check update

--- app/src/main/java/com/tistory/starcue/cuetalk/f1viewpager/F1F8.java
+++ app/src/main/java/com/tistory/starcue/cuetalk/f1viewpager/F1F8.java
@@ -1,5 +1,6 @@
 package com.tistory.starcue.cuetalk.f1viewpager;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
@@ -46,11 +47,33 @@ public class F1F8 extends Fragment {
             @Override
             public void onSuccess(DocumentSnapshot documentSnapshot) {
                 String url = documentSnapshot.get("f1f8").toString();
-                Log.d("F1F8>>>", url);
                 Uri uri = Uri.parse(url);
                 RequestOptions requestOptions = new RequestOptions();
                 requestOptions = requestOptions.transform(new CenterCrop(), new RoundedCorners(23));
                 Glide.with(F1F8.this).load(uri).override(300, 250).fitCenter().apply(requestOptions).into(imageView);
+            }
+        });
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                db.collection("f1viewpager").document("f1f8").get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        String where = documentSnapshot.get("where").toString();
+                        String url = documentSnapshot.get("url").toString();
+                        if (where.equals("internet")) {
+                            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                            intent.setPackage("com.android.chrome");//크롬으로 띄우기, 없으면 브라우저 선택
+                            startActivity(intent);
+                        } else if (where.equals("playstore")) {
+                            Intent intent = new Intent(Intent.ACTION_VIEW);
+                            intent.setData(Uri.parse("https://play.google.com/store/apps/details?id=com.tistory.starcue.bgnoise"));
+                            intent.setPackage("com.android.vending");
+                            startActivity(intent);
+                        }
+                    }
+                });
             }
         });
 
